4d98d81f603673204e76fedfc3c23c410b8994ea
Lum1natus <57365225+Lum1natus@users.noreply.github.com>
2021-08-19 20:24:49 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  var currentFloor = 2; // переменная с текущим этажом
+  var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
+  var counterUp = $(".counter-up");  // стрелка увеличения этажа
+  var counterDown = $(".counter-down"); // стрелка уменьшения этажа
+
+  //функция при наведении мышью на этаж
+  floorPath.on("mouseover", function () {
+    floorPath.removeClass('current-floor'); // удаляем активный класс у этажей
+    currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
+    $(".counter").text(currentFloor); // записываем значение этажа в счетчик
+  });
+
+  counterUp.on("click", function () {   // отслеживаем клик по кнопке вверх
+    if (currentFloor < 18) {            // проверяем значение этажа, не должно быть >18
+      currentFloor++;                   // прибавляем один этаж
+    usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });              // форматируем переменную с этажом
+      $(".counter").text(usCurrentFloor);  // записываем значение этажа в счетчик
+      floorPath.removeClass('current-floor'); // удаляем активный класс у этажей
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
+    }
+  })
+
+  counterDown.on('click', function () {
+    if (currentFloor > 2) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass('current-floor');
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  })
+});
